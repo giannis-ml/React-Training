84cfb721b96ec15f60dacd37f720fec2003be546
Ioannis Mallios <i.mallios@systemic.local>
2025-10-07 17:12:38 +0300

    Add context

--- Javascript/my-app/src/App.jsx
+++ Javascript/my-app/src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
+import { useState, createContext, useContext } from 'react'
 import './App.css'
 
+const boardContext = createContext(null)
+
+const BoardProvider = boardContext.Provider
 
 const Greeting = ({name}) => (
     <h1>Hello {name}</h1>
@@ -11,39 +14,56 @@ const MyButton = ({ trigger, label }) => (
     <button onClick={trigger} style={{ width: 100,height: 100}}>{label}</button>
 )
 
-function App() {
-    const [board, setBoard] = useState(Array(9).fill(""))
+const Board = () => {
 
-    const [playerXfirst, setPlayer] = useState(true)
-
-    const HandleGame = (i, label, symbol, xplayerNext) => (
-
-       ( <MyButton
-            key={i}
-            label={label}
-            trigger={() => {
-                setBoard(board.map((el, p) => (p === i ? symbol : el)));
-                setPlayer(xplayerNext);
-            }}
-        />)
-    )
+    const {handleClick,board} = useContext(boardContext)
 
     return (
-      <>
-        <div>
+        <>
+            <div>
                 <Greeting name="John" />
-                <div style={{display: 'grid',gridTemplateColumns: 'repeat(3, 100px)',gap: '10px',justifyContent: 'center',marginTop: '20px'}}>
+                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 100px)', gap: '10px', justifyContent: 'center', marginTop: '20px' }}>
                     {board.map((label, i) =>
-                        playerXfirst ? (
-                            HandleGame(i,label, 'x', false)
-                        ) : (
-                            HandleGame(i, label, 'o', true) 
-                        )
+                        <MyButton
+                            key={i}
+                            label={label}
+                            trigger={() => {
+                                handleClick(i)
+                            }}
+                        />
                     )}
                 </div>
-        </div>
-    </>
-  )
+            </div>
+        </>
+    )
+}
+
+
+function App() {
+
+    const [board, setBoard] = useState(Array(9).fill(null))
+
+    const [isXnext, setPlayer] = useState(true)
+
+    const handleClick = (position) => {
+
+        if (board[position]) 
+            return
+
+        const nextBoard = board.slice()
+        nextBoard[position] = isXnext ? 'o' : 'x'
+
+        setBoard(nextBoard)
+        setPlayer(!isXnext)
+    }
+
+    return (
+        <>
+            <BoardProvider value={{board,isXnext,handleClick}}>
+                    <Board />
+            </BoardProvider>
+        </>
+    )
 }
 
 export default App
